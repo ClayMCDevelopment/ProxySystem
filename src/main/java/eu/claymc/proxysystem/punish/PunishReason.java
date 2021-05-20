45d18900860beefa898872247ac8d1121a227774
McMaxDE <max.boeversen@gmx.de>
2021-05-20 20:14:45 +0200

    add more punish reasons

--- src/main/java/eu/claymc/proxysystem/punish/PunishReason.java
+++ src/main/java/eu/claymc/proxysystem/punish/PunishReason.java
@@ -5,7 +5,12 @@ import java.util.concurrent.TimeUnit;
 public enum PunishReason {
 
     HACKS(PunishType.BAN, TimeUnit.DAYS.toMillis(30)),
-    INSULT(PunishType.MUTE, TimeUnit.DAYS.toMillis(1));
+    INSULT(PunishType.MUTE, TimeUnit.DAYS.toMillis(1)),
+    REPORT_ABUSE(PunishType.BAN, TimeUnit.DAYS.toMillis(7)),
+    CHAT_ABUSE(PunishType.MUTE, TimeUnit.DAYS.toMillis(30)),
+    ADVERTISEMENT(PunishType.SHADOW_MUTE, TimeUnit.DAYS.toMillis(90))
+    ;
+
 
     private final PunishType type;
     private final long duration;
